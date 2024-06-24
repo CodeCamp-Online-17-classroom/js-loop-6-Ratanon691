8c9fd45b30af01115b0e652e21433e4c87861bcc
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-24 14:47:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-let num1 = +prompt(`Give me a number`)
-let num2 = +prompt(`Guess the number`)
-if(num2 == num1){
-  alert(`correct`)
-}
-else if(num2 < num1){
-  alert(`your number is too small`)
-}
-else if(num2 > num1){
-  alert(`your number is too big`)
+let num1
+do {
+  num1 = +prompt(`Give me a number between 0-100`)
+  if (num1 < 0 || num1 > 100) {
+    alert(`Please input a number between 0-100`)
+  }
+} while (num1 < 0 || num1 > 100);
+
+let num2 = +prompt(`Guess the number between 0-100`)
+if (num2 === num1) {
+  alert(`Correct`)
+} else if (num2 < num1) {
+  alert(`Your number is too small`)
+} else if (num2 > num1) {
+  alert(`Your number is too big`)
 }
