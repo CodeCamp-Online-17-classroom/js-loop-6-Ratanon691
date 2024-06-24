fb60700f443d372ebd3c71b89c76861af0b12308
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-24 14:00:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+let num1 = +prompt(`Give me a number`)
+let num2 = +prompt(`Guess the number`)
+if(num2 == num1){
+  alert(`correct`)
+}
+else if(num2 < num1){
+  alert(`your number is too small`)
+}
+else if(num2 > num1){
+  alert(`your number is too big`)
+}
